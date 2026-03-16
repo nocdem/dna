35d438dc6293fb8ac40e620fa18070270d8d9b0f
punk <punk@cpunk.io>
2026-03-16 13:24:14 +0300

    version: bump Nodus v0.9.14 (max value size 4MB)

--- nodus/include/nodus/nodus_types.h
+++ nodus/include/nodus/nodus_types.h
@@ -22,8 +22,8 @@ extern "C" {
 
 #define NODUS_VERSION_MAJOR  0
 #define NODUS_VERSION_MINOR  9
-#define NODUS_VERSION_PATCH  13
-#define NODUS_VERSION_STRING "0.9.13"
+#define NODUS_VERSION_PATCH  14
+#define NODUS_VERSION_STRING "0.9.14"
 
 /* Wire frame */
 #define NODUS_FRAME_MAGIC       0x4E44      /* "ND" */
