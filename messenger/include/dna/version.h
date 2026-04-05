22a8ea2ccb8deba982f8e62e19285ed5c045ffaf
punk <punk@cpunk.io>
2026-04-05 16:46:49 +0300

    chore: bump library version v0.9.165 -> v0.9.166 (TEE key wrapping)

--- messenger/include/dna/version.h
+++ messenger/include/dna/version.h
@@ -10,9 +10,9 @@
 
 #define DNA_VERSION_MAJOR 0
 #define DNA_VERSION_MINOR 9
-#define DNA_VERSION_PATCH 165
+#define DNA_VERSION_PATCH 166
 
-#define DNA_VERSION_STRING "0.9.165"
+#define DNA_VERSION_STRING "0.9.166"
 
 /* Build info (set by CMake) */
 #ifndef BUILD_HASH
